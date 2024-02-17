189a8470dec61077059a48202ae43595811a844a
Serghei <korsakov2975@gmail.com>
2024-02-17 12:22:07 +0300

    test failed

--- src/test/java/ru/netology/echo/PostmanEchoTest.java
+++ src/test/java/ru/netology/echo/PostmanEchoTest.java
@@ -1,1 +1,21 @@
 package ru.netology.echo;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostmanEchoTest {
+    @Test
+
+    void shouldReturnSendData() {
+        String text = "Hi";
+        given()
+                .baseUri("https://postman-echo.com/post")
+                .body(text)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo(text));
+    }
+}
